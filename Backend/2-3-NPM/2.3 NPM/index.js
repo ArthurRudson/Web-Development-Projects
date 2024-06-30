4ead75faa8b51a6093c027a3f0d05b09538253c5
ArthurRudson <154820615+ArthurRudson@users.noreply.github.com>
2024-06-30 09:23:25 -0300

    add the files

---
+++ Backend/2-3-NPM/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+//var generateName = require('sillyname');
+
+//import generateName from "sillyname"
+//var sillyName = generateName();
+
+import {randomSuperhero} from 'superheroes';
+
+
+console.log(`I'm ${randomSuperhero()}!`)
