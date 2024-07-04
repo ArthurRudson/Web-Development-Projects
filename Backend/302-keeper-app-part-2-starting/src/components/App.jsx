7d5165d993ee1985a4fd980b8d91c0abbd6beb02
ArthurRudson <154820615+ArthurRudson@users.noreply.github.com>
2024-07-04 16:58:46 -0300

    keeper app

--- Backend/302-keeper-app-part-2-starting/src/components/App.jsx
+++ Backend/302-keeper-app-part-2-starting/src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+     {notes.map(newNotes => (
+      <Note 
+      key={newNotes.key}
+      tit={newNotes.title}
+      cont={newNotes.content}
+      />
+     ))} 
       <Footer />
     </div>
   );
